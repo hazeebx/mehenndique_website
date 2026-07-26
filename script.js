78c02304e2345da3b58171af6e48b051db4b277d
hazeebx <hazeebx@gmail.com>
2026-07-26 16:55:54 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const path = document.querySelector("#drawingpath");
+const length = path.getTotalLength();
+
+path.style.fill = "none";
+path.style.strokeDasharray = length;
+path.style.strokeDashoffset = length;
+
+gsap.to(path, {
+    strokeDashoffset: 0,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".gallery",
+        start: "top 90%",
+        end: "+=1600",
+        scrub: true,
+        // markers: true
+    }
+});
